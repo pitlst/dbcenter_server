3e017ed57551654f8cdd611338e263dfe96d5442
cheakf <sunmoming@gmail.com>
2024-11-05 15:24:58 +0800

    更新调度器和sql与文件节点的写法

--- sync/scheduler.py
+++ sync/scheduler.py
@@ -3,6 +3,15 @@ from general.base import db_process_base  # 用于类型标注
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
+class scheduler:
+    '''dag调度器，支持动态调度'''
+    def __init__(self) -> None:
+        ...
+
+    def append_node(self):
+        ...
+
+
 def node_run(node_list: list[db_process_base]) -> None:
     '''启动线程池开始执行dag图'''
     node_map = process_name(node_list)
@@ -11,16 +20,16 @@ def node_run(node_list: list[db_process_base]) -> None:
         final_node = set()
         while True:
             run_node = get_node(final_node, node_map)
-            # 如果获取不到可以执行的节点即退出
-            if len(run_node) == 0:
-                break
             # 将节点添加到线程池
             for ch in run_node:
                 temp: db_process_base = node_list[node_map[ch]["index"]]
                 running_tasks.append(tpool.submit(temp.run))
             # 等待10s看任务是否完成，未完成则更新新的任务进行执行
-            for future in as_completed(running_tasks, timeout=5):
+            for future in as_completed(running_tasks, timeout=10):
                 final_node.add(future.result())
+            # 如果所有节点都执行完即算是同步完成
+            if len(final_node) == len(node_map):
+                break
 
 
 def process_name(node_list: list[db_process_base]) -> dict[str, dict[str, int | list[str]]]:
