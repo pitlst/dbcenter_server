23732ae4dbff691fe5b0bf536125506dc7ef4d73
cheakf <sunmoming@gmail.com>
2024-11-13 15:35:33 +0800

    换名与跑通mongo连接测试

--- process/tests/test_mongo_connect.cpp
+++ process/tests/test_mongo_connect.cpp
@@ -13,10 +13,10 @@ int main(int argc, char *argv[])
     mongocxx::uri uri{"mongodb://localhost:27017/"};
     // 创建一个client客户端
     mongocxx::client client = mongocxx::client{uri};
-    mongocxx::database db = client["dataframe"];
-    mongocxx::collection coll = db["config_connect"];
+    mongocxx::database db = client["logger"];
+    mongocxx::collection coll = db["网页自动化测试"];
     // 选择了数据库db，表coll
-    auto result = coll.find_one({});
-    std::cout << bsoncxx::to_json(*result) << std::endl;
+    auto result = coll.find_one({}).value();
+    std::cout << bsoncxx::to_json(result.view()) << std::endl;
     return 0;
 }
