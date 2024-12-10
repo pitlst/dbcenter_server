43a5a124f3f5e46f82bf45bf7b2f5a9372286b00
pitlst <sunmoming@gmail.com>
2024-12-10 22:39:41 +0800

    开始编写处理部分

---
+++ process/utils/pipeline.hpp
@@ -0,0 +1,17 @@
+#ifndef DBS_LOGGER_INCLUDE
+#define DBS_LOGGER_INCLUDE
+
+#include <memory>
+#include <string>
+
+#include "mongo.hpp"
+
+namespace dbs
+{
+    class pipeline
+    {
+        
+    };
+}
+
+#endif
