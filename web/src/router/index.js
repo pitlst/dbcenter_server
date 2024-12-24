d7821b514d0af6cd95ac4719425fd82128ba14e9
cheakf <sunmoming#gmail.com>
2024-12-24 16:04:41 +0800

    整理看板

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
-            name: 'home',
-            component: HomeView,
+            name: '主页',
+            component: () => import('../views/Home.vue'),
         },
         {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import('../views/AboutView.vue'),
+            path: '/travel',
+            name: '差旅看板',
+            component: () => import('../views/Travel.vue'),
+        },
+        {
+            path: '/DesignChange',
+            name: '设计变更统计',
+            component: () => import('../views/DesignChange.vue'),
         },
     ],
 })
