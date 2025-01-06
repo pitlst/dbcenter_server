e437b0449497670a74c97147747e0fbcbab71493
pitlst <sunmoming@gmail.com>
2025-01-07 00:20:18 +0800

    更新一些文档

--- process/tasks/person/person.cpp
+++ process/tasks/person/person.cpp
@@ -6,7 +6,7 @@ void task_person::main_logic()
 {
     // ----------从数据库读取数据----------
     LOGGER.info(this->node_name, "读取数据");
-
+    auto dwd_results = this->get_coll_data("ods", "shr_");
 
     
 }
